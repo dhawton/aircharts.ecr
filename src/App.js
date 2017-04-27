d6d75cb476fa806b0b437dcd4445a4c030745764
Daniel Hawton <daniel@hawton.com>
2017-04-27 15:40:37 -0800

    Fix useless constructor and unused variable warnings

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import SearchBox from './components/SearchBox.js';
 import './App.css';
 import './components/Loading.js';
 //api = require("./utils/api.js");
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div className="ECR">
