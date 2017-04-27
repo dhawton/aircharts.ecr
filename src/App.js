a0047a1dcbeb878c84c35e52fa3bf6f98569ae1c
Daniel Hawton <daniel@hawton.com>
2017-04-27 15:39:51 -0800

    Do proper component, maybe?

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import SearchBox from './components/SearchBox.js';
 import './App.css';
 import './components/Loading.js';
 //api = require("./utils/api.js");
 
-function SearchBar(props) {
-    return (
-        <p className="searchRow">
-            <input type="text" id="searchbox" placeholder="Airport Identifier"/>&nbsp;
-            <button className="btn-green" onClick={props.onSearch.bind(null)}>Search</button>
-        </p>
-    );
-}
-
-SearchBar.propTypes = {
-    identifier: PropTypes.string,
-    onSearch: PropTypes.func.isRequired
-};
-
 class App extends Component {
     constructor(props) {
-        super();
-        this.state = {
-            identifier: null
-        };
-
-        this.searchAirports = this.searchAirports.bind(this);
-    }
-
-    searchAirports() {
-        alert(document.getElementById("searchbox").value);
-        this.setState(function () {
-            return {
-                identifier: document.getElementById("searchbox").value.toUpperCase()
-            }
-        });
-        document.getElementById("searchbox").value = this.state.identifier;
-        alert("Hi " + this.state.identifier);
+        super(props);
     }
 
     render() {
@@ -45,7 +16,7 @@ class App extends Component {
                 <div className="ECR-header">
                     <h2>AirCharts ECR</h2>
                 </div>
-                <SearchBar identifier="" onSearch={this.searchAirports} />
+                <SearchBox />
             </div>
         );
     }
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,3 +1,36 @@
-/**
- * Created by Daniel on 4/26/2017.
- */
+import React, {Component} from 'react';
+import './SearchBox.css';
+
+class SearchBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            value: e.target.value
+        });
+    }
+
+    handleSubmit(e) {
+        console.log("Got submission: " + this.state.value);
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <p>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" id="searchbox" placeholder="Airport Identifier" value={this.state.value}/>&nbsp;
+                    <button className="btn-green" type="submit">Search</button>
+                </form>
+            </p>
+        )
+    }
+}
+
+export default SearchBox;
